80c031890cd773607dfe6865d442d8dc423ef692
Daniel <babipapi@gmail.com>
2020-01-04 15:22:54 +0800

    Initial Commit

---
+++ demopapperscissorstone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demopapperscissorstone
+//
+//  Created by Daniel on 2020/1/4.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
