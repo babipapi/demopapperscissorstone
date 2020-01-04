acfa4e3a0c018c4db0bb653e38f8c4f4d5d06e30
Daniel <babipapi@gmail.com>
2020-01-04 21:33:31 +0800

    fix

--- demopapperscissorstone/ViewController.swift
+++ demopapperscissorstone/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var upImageView: UIImageView!
+    @IBOutlet weak var downImageView: UIImageView!
+    
+    @IBOutlet weak var upLabel: UILabel!
+    
+    let names = ["papper","scissor","stone"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        upLabel.text = "開始玩吧，你先選一個～"
     }
+    
+    @IBAction func scissorButton(_ sender: UIButton) {
+        downImageView.image = UIImage(named: "scissor")
+        upImageView.image = UIImage(named: "")
+        upLabel.text = ""
+        aiplay()
+        whowin()
+    }
+    
+    @IBAction func stoneButton(_ sender: UIButton) {
+        downImageView.image = UIImage(named: "stone")
+        upImageView.image = UIImage(named: "")
+        upLabel.text = ""
+        aiplay()
+        whowin()
+    }
+    
+    @IBAction func papperButton(_ sender: UIButton) {
+        downImageView.image = UIImage(named: "papper")
+        upImageView.image = UIImage(named: "")
+        upLabel.text = ""
+        aiplay()
+        whowin()
+    }
+    
+    func aiplay(){
+           let randomname = names.randomElement()!
+            upImageView.image = UIImage(named: "\(randomname)")
+       }
 
-
+    func whowin(){
+        if upImageView.image == UIImage(named: "scissor"),downImageView.image == UIImage(named: "scissor"){
+            upLabel.text = "平手"
+        }else if upImageView.image == UIImage(named: "papper"),downImageView.image == UIImage(named: "papper"){
+        upLabel.text = "平手"
+        }else if upImageView.image == UIImage(named: "stone"),downImageView.image == UIImage(named: "stone"){
+        upLabel.text = "平手"
+        }else if upImageView.image == UIImage(named: "stone"),downImageView.image == UIImage(named: "papper"){
+        upLabel.text = "你贏了"
+        }else if upImageView.image == UIImage(named: "stone"),downImageView.image == UIImage(named: "scissor"){
+        upLabel.text = "電腦贏了"
+        }else if upImageView.image == UIImage(named: "papper"),downImageView.image == UIImage(named: "scissor"){
+        upLabel.text = "你贏了"
+        }else if upImageView.image == UIImage(named: "papper"),downImageView.image == UIImage(named: "stone"){
+        upLabel.text = "電腦贏了"
+        }else if upImageView.image == UIImage(named: "scissor"),downImageView.image == UIImage(named: "stone"){
+        upLabel.text = "你贏了"
+        }else if upImageView.image == UIImage(named: "scissor"),downImageView.image == UIImage(named: "papper"){
+        upLabel.text = "電腦贏了"
+        }
+    }
 }
 
